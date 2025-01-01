11f978d306aed3f61ea0895f5dfbbee7cc1cc06e
Santiago Diaz <sddiaz@shockers.wichita.edu>
2025-01-01 15:16:41 -0600

    Updates ESLINT rules.

--- amazon-clone/eslint.config.mjs
+++ amazon-clone/eslint.config.mjs
@@ -13,7 +13,8 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      "react/no-unescaped-entities": "off"
+      "react/no-unescaped-entities": "off",
+      "@typescript-eslint/no-unused-vars": "off"
     }
   }
 ];
